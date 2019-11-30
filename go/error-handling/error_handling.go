3e30cc2061573cb437260aad1a814247c08af76d
john <johntryan@hotmail.com>
2019-11-30 09:48:45 +0000

    test no errors working

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,1 +1,18 @@
 package erratum
+
+import(
+	"errors"
+)
+
+// open and use a resource
+func Use(o ResourceOpener, input string) error{
+	res, err := o()
+	if nil != err {
+		// presume this means the resource was not opened
+		return errors.New("fixme")
+	}else{
+		defer res.Close()
+		res.Frob(input)
+		return nil
+	}
+}
