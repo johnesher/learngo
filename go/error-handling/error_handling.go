e844d37fddd319b6c62a1c615cbad1acc5044630
john <johntryan@hotmail.com>
2019-12-01 09:35:45 +0000

    as submitted

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -16,16 +16,18 @@ func Use(o ResourceOpener, input string) (retval error) {
 	// opened ok
 	defer res.Close()
 	defer func () {
-		if r := recover(); r != nil {
-			switch v := r.(type) {
-			case FrobError:
-				res.Defrob(v.defrobTag)
-				retval = v.inner
-			default:
-				retval = v.(error)
-			}
+		switch v := recover().(type) {
+		case FrobError:
+			res.Defrob(v.defrobTag)
+			retval = v.inner
+		case error:
+			retval = v
+		default:
+			// ignore
 		}
 	}()
+	// Finally call the target
 	res.Frob(input)
-	return nil
+	retval = nil
+	return  // retval
 }
